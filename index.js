3f5719325d15249a10b8a42aa6df8a1e0184821e
Dipanjan Panja <dipanjanpanja6@gmail.com>
2020-07-30 18:21:34 +0530

    l

--- index.js
+++ index.js
@@ -22,19 +22,19 @@ var whitelist = [
   'http://tutor.qrioctybox.com', 
   'http://localhost',
 ]
-// app.use(cross({
-//   origin: function (origin, callback) {
-//     // allow requests with no origin 
-//     // (like mobile apps or curl requests)
-//     if (!origin) return callback(null, true);
-//     if (whitelist.indexOf(origin) === -1) {
-//       var msg = 'The CORS policy for this site does not ' +
-//         'allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }, credentials: true
-// }));
+app.use(cross({
+  origin: function (origin, callback) {
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (whitelist.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }, credentials: true
+}));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
