43ff9be303a572e4ff80341a6234a609922c4b5d
Dipanjan Panja <48008098+dipanjanpanja6@users.noreply.github.com>
2021-06-17 01:10:52 +0530

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -3,14 +3,8 @@ const firebase = require('firebase')
 
 
 var firebaseConfig = {
-  apiKey: "AIzaSyB8FA_XLN3p61LjiodWTkOIVAKSqTbZRYw",
-  authDomain: "q-box-client-fa30b.firebaseapp.com",
-  databaseURL: "https://q-box-client-fa30b.firebaseio.com",
-  projectId: "q-box-client-fa30b",
-  storageBucket: "q-box-client-fa30b.appspot.com",
-  messagingSenderId: "568701074451",
-  appId: "1:568701074451:web:b1a46b75575325c13e93e6",
-  measurementId: "G-W2FW4Z1H8J"
+  apiKey: "",
+  projectId: "q-box-client-fa30b"
 };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig); 
